e3182ba4319610944c51c564cf36cd37ec2880af
Ryder B. <153897299+Ryder-Blase@users.noreply.github.com>
2025-11-15 16:48:42 +0100

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -4,7 +4,7 @@ taskkill /f /im RTSS.exe >nul 2>&1
 taskkill /f /im EncoderServer.exe >nul 2>&1
 taskkill /f /im RTSSHooksLoader64.exe >nul 2>&1
 timeout /t 1 >nul 2>&1
-C:\WinMemoryCleaner.exe /ModifiedPageList /ProcessesWorkingSet /StandbyList /SystemWorkingSet >nul 2>&1
-timeout /t 1 >nul 2>&1
-C:\WinMemoryCleaner.exe /ModifiedPageList /ProcessesWorkingSet /StandbyList /SystemWorkingSet >nul 2>&1
+C:\WinMemoryCleaner.exe /ModifiedPageList /CombinedPageList /ModifiedFileCache /ProcessesWorkingSet /RegistryCache /StandbyList /SystemWorkingSet /SystemFileCache /WorkingSet >nul 2>&1
+C:\WinMemoryCleaner.exe /ModifiedPageList /CombinedPageList /ModifiedFileCache /ProcessesWorkingSet /RegistryCache /StandbyList /SystemWorkingSet /SystemFileCache /WorkingSet >nul 2>&1
+
 
